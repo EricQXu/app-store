559014efcd17b0ff6e2771ece861a72e71254fe1
Adwaith R Madadi <adwaith@eduroam-169-233-222-194.ucsc.edu>
2024-01-21 07:58:09 -0800

    polished the appListing

---
+++ appListings/steam/windows/install_sublime.ps1
@@ -0,0 +1,10 @@
+<# 
+	no need to run sudo
+	run in Terminal: "set-executionpolicy bypass -scope process" (without quotes)
+	run in Terminal: "powershell -file %USERPROFILE%\Downloads\test.ps1" (without quotes) 
+#>
+if exist "C:\Program Files\Sublime Text\sublime_text.exe" (
+    echo "Sublime Text 4 is already installed on this system"
+) else (
+    echo "Installing Sublime Text 4..."; winget install --id=Valve.Steam  -e; echo "Sublime Text 4 installed"
+)
