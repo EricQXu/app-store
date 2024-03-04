c8d04e6c4c021f89318b1b7f92872d13c9c6364e
ericxu <ericxuhello3@gmail.com>
2024-03-03 17:15:15 -0800

    added adobe installer links (valid license required)

---
+++ static/appListings/photoshop/windows/install_photoshop.ps1
@@ -0,0 +1,11 @@
+<# 
+	no need to run sudo
+	run in Terminal: "set-executionpolicy bypass -scope process" (without quotes)
+	run in Terminal: "powershell -file %USERPROFILE%\Downloads\test.ps1" (without quotes) 
+	download offline installer from https://creativecloud.adobe.com/cc
+#>
+if exist "C:\Program Files\Sublime Text\sublime_text.exe" (
+    echo "Sublime Text 4 is already installed on this system"
+) else (
+    echo "Installing Sublime Text 4..."; winget install --id=SublimeHQ.SublimeText.4  -e; echo "Sublime Text 4 installed"
+)
