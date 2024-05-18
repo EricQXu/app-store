39f1505dff8742ac407c32b97f784be6beb02d1a
EricQXu <ericxuhello3@gmail.com>
2024-05-18 11:11:31 -0700

    appListing for chrome - navbar change

--- static/appListings/chrome/script.js
+++ static/appListings/chrome/script.js
@@ -71,7 +71,6 @@ function getOS() {
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
