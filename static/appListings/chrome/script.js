69fcbbd0bcb2d070566073cdb189dd96a41066e6
EricQXu <ericxuhello3@gmail.com>
2024-05-30 01:38:23 -0700

    fixed discord and ms365

--- static/appListings/chrome/script.js
+++ static/appListings/chrome/script.js
@@ -79,9 +79,6 @@ toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
 
-// searchBtn.addEventListener("click" , () =>{
-//     sidebar.classList.remove("close");
-// })
 
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
--- static/appListings/discord/script.js
+++ static/appListings/discord/script.js
@@ -71,7 +71,6 @@ function getOS() {
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
@@ -80,9 +79,6 @@ toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
 
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
